18beae86e56d9f65de39958dd7c37ca2887e5772
walleyjs <rajiolawale2020@gmail.com>
2019-10-14 22:33:31 +0100

    more more updates

--- drop/drop.js
+++ drop/drop.js
@@ -1,43 +1,31 @@
-const carouselSlide = document.querySelector(".carousel-slide");
-const carouselImages = document.querySelectorAll(".carousel-slide img");
-const prveBtn = document.querySelector("#prevBtn");
-const nextBtn = document.querySelector("#nextBtn");
+console.log("please work");
+var slideIndex = 1;
+showSlides(slideIndex);
 
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
 
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
 
-let counter = 1;
-const size = carouselImages[0].clientWidth;
-carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
-
-
-nextBtn.addEventListener("click", () => {
-    if (counter >= carouselImages.length - 1) return;
-    carouselSlide.style.transition = "transform 0.4s ease-in-out";
-    counter++;
-    console.log(counter);
-    carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
-})
-prevBtn.addEventListener("click", () => {
-    if (counter <= 0) return;
-    carouselSlide.style.transition = "transform 0.4s ease-in-out";
-    counter--;
-
-    carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
-})
-
-carouselSlide.addEventListener("transitionend", () => {
-    if (carouselImages[counter].id === "lastclone") {
-        carouselSlide.style.transition = "none";
-        counter = carouselImages.length - 2;
-        console.log(counter);
-
-        carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
-
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1
     }
-    if (carouselImages[counter].id === "firstclone") {
-        carouselSlide.style.transition = "none";
-        counter = carouselImages.length - counter;
-        carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
-
+    if (n < 1) {
+        slideIndex = slides.length
     }
-})
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
